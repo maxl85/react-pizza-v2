d3d74c539010a1ce007d22a08fd49fc6352f6f3d
maxl85 <maxl85@mail.ru>
2022-07-10 21:48:03 +0300

    useState

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+// import logo from './logo.svg';
+import './App.css';
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+import pizzas from './assets/pizzas.json';
+
+
+function App() {
+    return (
+        <div className="wrapper">
+            <Header />
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories />
+                        <Sort />
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        {
+                            /* <PizzaBlock title="Мексиканская" price={500} /> */
+                            pizzas.map((obj) => (
+                                <PizzaBlock name={obj.name} price={obj.price} imageUrl={obj.imageUrl} />
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
