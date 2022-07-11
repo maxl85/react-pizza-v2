45f8b119906c2fdc16b02e8930933a03764f5418
maxl85 <maxl85@mail.ru>
2022-07-11 19:53:16 +0300

    Server search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,9 +20,10 @@ const Home = ({ searchValue }) => {
         const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
         const sortBy = sortType.sortProperty.replace('-', '');
         const category = categoryId > 0 ? `category=${categoryId}` : '';
+        const search = searchValue ? `&search=${searchValue}` : '';
 
         fetch(
-            `https://62cb3b881eaf3786ebb710bc.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+            `https://62cb3b881eaf3786ebb710bc.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search}`,
         )
             .then((res) => res.json())
             .then((arr) => {
@@ -30,16 +31,9 @@ const Home = ({ searchValue }) => {
                 setIsLoading(false);
             });
         window.scrollTo(0, 0);
-    }, [categoryId, sortType]);
+    }, [categoryId, sortType, searchValue]);
 
-    const pizzas = items
-        .filter((obj) => {
-            if (obj.name.toLowerCase().includes(searchValue.toLowerCase())) {
-                return true;
-            }
-            return false;
-        })
-        .map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+    const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
 
     const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
 
